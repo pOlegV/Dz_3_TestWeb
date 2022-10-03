ee865547551b10bce330544e8ff174cb899250aa
pOlegV <oleg-1@yandex.ru>
2022-10-03 11:15:14 +0300

    Add_Test

---
+++ src/test/java/WebAppTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WebAppTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setAppAll(){
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setApp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+
+    @Test
+    public void test(){
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Юрий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79874561232");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[role=button]")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @AfterEach
+    void closeWeb(){
+        driver.close();
+        driver = null;
+    }
+}
